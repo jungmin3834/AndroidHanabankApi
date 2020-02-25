6ebb7bf0e3632769830581d66b9915ac5074b64c
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-02-25 14:16:04 +0900

    String * String 함수 제작 및 받아온 데이터를 바탕으로 미국 달러 -> 원화로 환율 변경 성공

--- app/src/main/java/com/example/globalexchangerate/MainActivity.kt
+++ app/src/main/java/com/example/globalexchangerate/MainActivity.kt
@@ -4,11 +4,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import org.json.JSONArray
-import java.net.URL
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
 
     val container = Container(HashMap())
 
@@ -19,10 +19,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun init(){
-        Server().getRateDataFromServer(container)
+        Server(container,"http://172.30.1.17:8080/getMoneyRate").execute()
+    }
+
+
+    fun calculateMoneyRate(money : String): String {
+        var m = StringBuffer(money)
+        var r = container.moneyRateList.get(beforeTransfer_Tv.text.toString())?.money
+        var rate: Int
+        if( r != null){
+            var ratestr = StringBuffer("")
+            var count = 0
+            for(s in  r.toString()) {
+                if(s != '.') {
+                    ratestr.append(s)
+                    count++
+                }
+                else
+                    count = 0
+            }
+            if(count ==  r.toString().length)
+                count = 0
+            rate = ratestr.toString().toInt()
+
+            var num = 0
+            for (i in m.length - 1 downTo 0) {
+                num += (m[i] - '0') * rate
+                m.setCharAt(i ,if (num > 9) (num % 10 + '0'.toInt()).toChar() else (num + '0'.toInt()).toChar())
+                num /= 10
+            }
+
+            while (num > 0) {
+                m.insert(0,(num % 10 + '0'.toInt()).toChar())
+                num /= 10
+            }
+            m.insert(m.length- count , '.')
+            return m.toString()
+        }
+        return ""
     }
 
     fun clickOnTransferMoney(view: View) {
-
+        Log.v("메세지 : " , beforeTransfer_Tv.text.toString())
+        afterTransfer_tv.setText(calculateMoneyRate(beforeTransfer_et.text.toString()) + " " +
+                afterCountry_tv.text)
     }
 }
--- app/src/main/java/com/example/globalexchangerate/Server.kt
+++ app/src/main/java/com/example/globalexchangerate/Server.kt
@@ -1,18 +1,25 @@
 package com.example.globalexchangerate
 
-import android.os.Build
+import android.os.AsyncTask
 import android.util.Log
-import androidx.annotation.RequiresApi
 import org.json.JSONArray
-import org.json.JSONObject
-import java.net.HttpURLConnection
 import java.net.URL
 
-class Server(){
-    fun getRateDataFromServer(container : Container){
+
+class Server(container : Container, path : String) : AsyncTask<Void, Void, Void>() {
+    val container = container
+    val path = path
+
+    override fun doInBackground(vararg params: Void?): Void? {
+        getRateDataFromServer()
+        return null
+    }
+
+    fun getRateDataFromServer(){
         val task = Runnable {
-            val jsonStr = URL("http://172.30.1.17:8080/getMoneyRate").readText()
+            val jsonStr = URL(path).readText()
             var js = JSONArray(jsonStr)
+            Log.v("성공 여부",jsonStr)
             for (i in 0 until js.length()) {
                 val item = js.getJSONObject(i)
                 container.moneyRateList.getOrPut(item.get("name").toString()){ RateData(
@@ -21,8 +28,14 @@ class Server(){
                     item.get("money").toString().toFloat())
                 }
             }
-            Log.v("성공 여부",jsonStr)
+
         }
         task.run()
     }
+
 }
+
+
+
+
+
