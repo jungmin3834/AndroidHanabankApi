faaa5e6614d345c6a0294c4d1739bc4ada53a058
jungmin3834 <52225582+jungmin3834@users.noreply.github.com>
2020-03-03 16:20:51 +0900

    getRateSplit 함수 추가 기존 CountryName + Rate 으로 예) 호주 달러 AUD로 이루어진 spinner에서 AUD만 추출 해주는 String 반환 함수

--- app/src/main/java/com/example/globalexchangerate/MainActivity.kt
+++ app/src/main/java/com/example/globalexchangerate/MainActivity.kt
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_country_list.view.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 
 class MainActivity : AppCompatActivity() {
@@ -35,7 +36,7 @@ class MainActivity : AppCompatActivity() {
             }
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
                 if(beforeTransfer_et.text.toString().length == 0)
-                    afterTransfer_tv.text = "0 " +  beforeTransfer_sp.toString()
+                    afterTransfer_tv.text = "0 " +  getRateSplit(beforeTransfer_sp.selectedItem.toString())
                 else
                     afterTransfer_tv.text = calculateMoneyRate(beforeTransfer_et.text.toString()) + " " + afterCountry_sp.toString()
             }
@@ -45,7 +46,7 @@ class MainActivity : AppCompatActivity() {
 
     fun calculateMoneyRate(money : String): String {
         var m = StringBuffer(money)
-        var r = container.moneyRateList.get(beforeTransfer_sp.toString())?.money
+        var r = container.moneyRateList.get(getRateSplit(beforeTransfer_sp.selectedItem.toString()))?.money
         var rate: Int
         if( r != null){
             var ratestr = StringBuffer("")
@@ -79,12 +80,18 @@ class MainActivity : AppCompatActivity() {
         return ""
     }
 
+    fun getRateSplit(countryRate : String) : String{
+      var str = StringBuilder(countryRate)
+      return "" + str.get(str.length - 3)  + str.get(str.length - 2)  + str.get(str.length - 1)
+
+    }
+
     fun clickOnTransferMoney(view: View) {
         Log.v("메세지 : " , beforeTransfer_sp.toString())
         if(beforeTransfer_et.text.toString().length == 0)
-            afterTransfer_tv.text = "0 " +  afterCountry_sp.toString()
+            afterTransfer_tv.text = "0 " +  getRateSplit(afterCountry_sp.selectedItem.toString())
         else
             afterTransfer_tv.text = calculateMoneyRate(beforeTransfer_et.text.toString()) + " " +
-                afterCountry_sp.toString()
+                    getRateSplit(afterCountry_sp.selectedItem.toString())
     }
 }
